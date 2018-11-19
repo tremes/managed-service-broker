0df67193894268e083faf65113d708a82ebfc8c0
Tomáš Remeš <tremes@redhat.com>
2018-11-19 09:23:52 +0100

    next

--- scripts/docker_build_and_push.sh
+++ scripts/docker_build_and_push.sh
@@ -5,5 +5,5 @@ APP_NAME=managed-service-broker
 IMAGE_MASTER_TAG=quay.io/integreatly/$APP_NAME:latest
 
 docker login --username $REGISTRY_USERNAME --password $REGISTRY_PASSWORD $REGISTRY_HOST
-docker build -t $IMAGE_MASTER_TAG .
+docker build -t $IMAGE_MASTER_TAG -f ./tmp/build/broker/Dockerfile .
 docker push $IMAGE_MASTER_TAG
